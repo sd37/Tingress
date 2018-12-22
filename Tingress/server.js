c1490b1852c70b719a0affaeeb5723142410880a
Spandan Dutta <spdutta@microsoft.com>
2018-12-21 21:20:11 -0800

    eslint corrections

--- Tingress/server.js
+++ Tingress/server.js
@@ -1,13 +1,15 @@
-'use strict';
+'use strict'
 import { TinderClient } from 'tinder-client'
 
-var http = require('http');
-var port = process.env.PORT || 1337;
+var http = require('http')
+var port = process.env.PORT || 1337
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Hello World\n')
+}).listen(port)
+
+const facebookUserId = 'someFacebookUserId'
+const facebookToken = 'someFacebookToken'
 
 var client = TinderClient.create({ facebookUserId, facebookToken })
-
