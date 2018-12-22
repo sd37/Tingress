e64470e49de3c6d5b282d0cb9e98273daec9c2b4
Spandan Dutta <spdutta@microsoft.com>
2018-12-21 21:21:36 -0800

    http stuff deleted

--- Tingress/server.js
+++ Tingress/server.js
@@ -1,14 +1,6 @@
 'use strict'
 import { TinderClient } from 'tinder-client'
 
-var http = require('http')
-var port = process.env.PORT || 1337
-
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World\n')
-}).listen(port)
-
 const facebookUserId = 'someFacebookUserId'
 const facebookToken = 'someFacebookToken'
 
