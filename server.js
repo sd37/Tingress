12f2a6b203aa0e5570bc855b4693d1cbb4805900
Spandan Dutta <spdutta@microsoft.com>
2018-12-23 15:35:23 -0800

    more timer adjustments

--- server.js
+++ server.js
@@ -60,15 +60,13 @@ async function likeAndPushToDb (client) {
 }
 
 async function sleepyLike (client, id) {
-  let secondsToSleep = 1
+  let secondsToSleep = 0.5
   sleep(secondsToSleep * 1000)
   let likeOutput = await client.like(id)
   console.log(likeOutput)
 }
 
 async function sleepyGetRecommendations (client) {
-  let secondsToSleep = 0.5
-  sleep(secondsToSleep * 1000)
   let recosResponses = await client.getRecommendations()
   return recosResponses
 }
