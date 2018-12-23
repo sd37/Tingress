e15aabf5bb1d9b8c5143957b4e0ccb01cb17298e
Spandan Dutta <spdutta@microsoft.com>
2018-12-22 18:00:45 -0800

    working tinder client. Can retrieve basic profile info.

--- server.js
+++ server.js
@@ -3,11 +3,21 @@
 import { TinderClient } from 'tinder-client'
 let fs = require('fs')
 
-async function getClient (id, token) {
-  let client = await TinderClient.create({ id, token })
+async function getClient (facebookUserId, facebookToken) {
+  let client = null
+  try {
+    client = await TinderClient.create({ facebookUserId, facebookToken })
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+
   return client
 }
 
+// How to get tinder access token:
+// https://github.com/defaultnamehere/tinder-detective/issues/3
+// https://medium.com/@paulxuca/tinder-tales-or-the-search-for-tinders-new-api-4d3a36e2542
 function getCredentials () {
   let content = fs.readFileSync('./credentials.json')
   let jsonContent = JSON.parse(content)
@@ -18,6 +28,8 @@ function getCredentials () {
 async function main () {
   let cred = getCredentials()
   let client = await getClient(cred.userId, cred.token)
+  let profile = await client.getProfile()
+  console.log(profile)
 }
 
 // the server starts here..
